06e33e53ae53552949a71422865d2c73ff170204
Sunao-Yoshii <2810066+Sunao-Yoshii@users.noreply.github.com>
2026-06-01 23:01:02 +0900

    README およびバージョン更新

--- backend/api/default_template_api.py
+++ backend/api/default_template_api.py
@@ -28,7 +28,7 @@ class DefaultTemplateApi:
             message="StableDiffusion WebUI 特化画像ビューワー.",
             data={
                 "name": "AZViewer",
-                "version": "1.4.0",
+                "version": "1.5.0",
                 "description": "StableDiffusion WebUI 特化画像ビューワー.",
             },
         ).to_dict()
